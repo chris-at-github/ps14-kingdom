27ef5ab7b2009940bb6c953563a31c48fca11753
Christian Pschorr <c.pschorr@datamints.com>
2021-02-08 23:04:38 +0100

    0.0.1@2021-02-08

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,20 +11,20 @@
  ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = [
-    'title' => 'Kingdom',
-    'description' => '',
-    'category' => 'plugin',
-    'author' => 'Christian Pschorr',
-    'author_email' => 'pschorr.christian@gmail.com',
-    'state' => 'alpha',
-    'createDirs' => '',
-    'clearCacheOnLoad' => 0,
-    'version' => '1.0.0',
-    'constraints' => [
-        'depends' => [
-            'typo3' => '10.4.0-10.4.99',
-        ],
-        'conflicts' => [],
-        'suggests' => [],
-    ],
+	'title' => 'Kingdom',
+	'description' => '',
+	'category' => 'plugin',
+	'author' => 'Christian Pschorr',
+	'author_email' => 'pschorr.christian@gmail.com',
+	'state' => 'alpha',
+	'createDirs' => '',
+	'clearCacheOnLoad' => 0,
+	'version' => '1.0.0',
+	'constraints' => [
+		'depends' => [
+			'typo3' => '10.4.0-10.4.99',
+		],
+		'conflicts' => [],
+		'suggests' => [],
+	],
 ];
